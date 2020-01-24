8c12d64f189dd25197e3eb19b778cd07cd4bd1a1
Joncarlos Perdomo <techsavage23@gmail.com>
2020-01-24 13:29:34 -0500

    Update minWindow.java

--- leetcode/String/minWindow.java
+++ leetcode/String/minWindow.java
@@ -1,39 +1,47 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] map = new int[128];
-        for(char c: t.toCharArray()) {
-            map[c]++;
-        }
-        
-        int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, counter = t.length();
-        
-        while(end < s.length()) {
-            char c1 = s.charAt(end);
-            if(map[c1] > 0) {
-                counter--;
-            }
-            map[c1]--;
-            end++;
-            
-            while(counter == 0) {
-                if(minLen > end - start) {
-                    minLen = end - start;
-                    minStart = start;
-                }
-                
-                char c2 = s.charAt(start);
-                map[c2]++;
-                if(map[c2] > 0) {
-                    counter++;
-                }
-                start++;
-            }
-        }
-        
-        if(minLen == Integer.MAX_VALUE) {
+        if(t.length() > s.length()) {
             return "";
         }
         
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        int counter = map.size();
+        
+        int begin = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE;
+        
+        while(end < s.length()) {
+            char c1 = s.charAt(end);
+            if(map.containsKey(c1)) {
+                map.put(c1, map.get(c1) - 1);
+                if(map.get(c1) == 0) {
+                    counter--;
+                }
+            }
+            end++;
+            
+            while(counter == 0) {
+                char c2 = s.charAt(begin);
+                if(map.containsKey(c2)) {
+                    map.put(c2, map.get(c2) + 1);
+                    if(map.get(c2) > 0) {
+                        counter++;
+                    }
+                }
+                
+                if(minLen > end - begin) {
+                    minLen = end - begin;
+                    minStart = begin;
+                }
+                begin++;
+            }
+        }
+        
+        if(minLen == Integer.MAX_VALUE)
+            return "";
         return s.substring(minStart, minStart + minLen);
     }
 }
